cff604098259d6384a043d3f39f8b75c8a9159ae
peng  chen <39702717+peoplhappy@users.noreply.github.com>
2019-05-08 23:12:55 +0800

    导入elementui
    导入elementui

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-progress type="circle" :percentage="0"></el-progress>
+    <el-progress type="circle" :percentage="25"></el-progress>
+    <el-progress type="circle" :percentage="100" status="success"></el-progress>
+    <el-progress type="circle" :percentage="50" status="exception"></el-progress>
   </div>
 </template>
-
+ 
 <script>
 export default {
-  name: 'App'
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
