f93dfe4e696b9c84fea2ed42a4dec3523e0d3526
DoubleYeah <31724867+DoubleYeah@users.noreply.github.com>
2019-05-12 18:12:27 +0800

    增加nav栏 增加dashboard页
    增加nav栏
    首页 看板（下拉菜单）
    增加dashboard页
    左树右详情

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <el-progress type="circle" :percentage="0"></el-progress>
-    <el-progress type="circle" :percentage="25"></el-progress>
-    <el-progress type="circle" :percentage="100" status="success"></el-progress>
-    <el-progress type="circle" :percentage="50" status="exception"></el-progress>
+    <el-menu default-active="helloworld" class="el-menu-demo" mode="horizontal" @select="handleSelect" router>
+      <el-menu-item index="helloworld">HelloWorld</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Dashboard</template>
+        <el-menu-item index="dashboard#1">dashboard1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>无效按钮</el-menu-item>
+      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Link</a></el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+    <router-view></router-view>
   </div>
 </template>
  
@@ -14,8 +28,17 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleSelect (link) {
+      console.log(link)
     }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,58 @@
+<template>
+    <el-container>
+        <el-aside>
+                <left-tree></left-tree>
+        </el-aside>
+        <el-container>
+            <el-header></el-header>
+            <el-main></el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+import LeftTree from './LeftTree'
+
+export default {
+    name: "Dashboard",
+    data () {
+        return {
+
+        };
+    },
+    components: {
+        'left-tree': LeftTree
+    }
+}
+</script>
+
+<style scoped>
+.el-container{
+    /* box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04) */
+}
+.el-header {
+  background-color: #B3C0D1;
+  text-align: center;
+  color: #333;
+  line-height: 1.5em;
+  border: solid #dcdfe6;
+  border-width: 0 1px 0 1px;
+}
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 1.5em;
+    height: 450px;
+    border: solid #dcdfe6;
+    border-width: 0 1px 1px 1px;
+}
+.el-aside{
+    color:#333;
+    border: solid #dcdfe6;
+    border-width: 0 0 1px 1px;
+}
+.el-tree, .el-aside{
+    background-color: #E4E7ED;
+}
+</style>
