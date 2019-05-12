8b70234a5c56c7f08cb3a9b22213a41e47525982
peng  chen <39702717+peoplhappy@users.noreply.github.com>
2019-05-13 00:23:14 +0800

    提交foreachcontroller.vue
    提交foreachcontroller.vue

---
+++ src/components/controller/ForeachController.vue
@@ -0,0 +1,60 @@
+<template>
+       <el-container>
+        <el-header height="10px">Foreach Controller</el-header>
+        <el-main>
+             <div class="baseproperty">
+                 <el-input v-model="input">
+                    <template slot="prepend">Name:</template>                
+                 </el-input>
+                 <el-input v-model="input2" placeholder="">
+                     <template slot="prepend">comments:</template> 
+                 </el-input>
+             </div>
+             <div class="testelementproperty">
+                  <el-input v-model="input2">
+                    <template slot="prepend">Input variable prefix</template>                
+                  </el-input>
+                  <el-input v-model="input2">
+                    <template slot="prepend">Start index for loop(exclusive)</template>                
+                  </el-input>
+                  <el-input v-model="input2">
+                    <template slot="prepend">End index for loop(inclusive)</template>                
+                  </el-input>
+                  <el-input v-model="input2">
+                    <template slot="prepend">Output variable name</template>                
+                  </el-input>
+                   <el-checkbox label="Add   '_'   before   number?"></el-checkbox>
+             </div>       
+        </el-main>       
+      </el-container>
+    
+</template>
+
+<script>
+export default {
+    name: "ForeachController",
+    data () {
+        return {
+            input:"ForEach Controller",
+            input2:""
+        };
+    },
+    
+}
+</script>
+
+<style>
+  .el-main{
+     margin-top:10px
+  }
+  div .baseproperty{
+      border: solid #dcdfe6;
+  }
+  div .testelementproperty{
+      margin-top:10px;
+      border: solid #dcdfe6;
+  }
+  .el-checkbox{
+      margin-top:10px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard'
+import ForeachController from '@/components/controller/ForeachController'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/dashboard',
       name: "Dashboard",
       component: Dashboard
+    },
+    {
+      path: '/controller/ForeachController',
+      name: "ForeachController",
+      component: ForeachController
     }
   ]
 })
