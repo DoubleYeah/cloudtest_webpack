a1d854f9b414435a289ed8a0c7d9fed2e0eb013a
peng  chen <39702717+peoplhappy@users.noreply.github.com>
2019-05-20 23:27:00 +0800

    去掉头，避免其余的组件也加载
    去掉头，避免其余的组件也加载

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,19 @@
-
 <template>
-  <div class="hello">
-    <el-menu default-active="helloworld" class="el-menu-demo" mode="horizontal" @select="handleSelect" router>
-      <el-menu-item index="helloworld">HelloWorld</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">Dashboard</template>
-        <el-menu-item index="dashboard#1">dashboard1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>无效按钮</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Link</a></el-menu-item>
-    </el-menu>
-    <div class="line"></div>
-    <router-view></router-view>
-  </div>
+  <div id="app"><router-view /></div>
 </template>
- 
+
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  methods: {
-    handleSelect (link) {
-      console.log(link)
-    }
-  }
-}
+  name: "App"
+};
 </script>
 
 <style scoped>
-
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #ecf0f5;
+}
 </style>
