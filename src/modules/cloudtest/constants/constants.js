4f4daecf7b1a37bbfd29f56e2341ba9a7a6bec7d
peoplhappy <39702717+peoplhappy@users.noreply.github.com>
2019-05-29 13:56:51 +0800

    更新组件的常量定义
    更新组件的常量定义

--- src/modules/cloudtest/constants/constants.js
+++ src/modules/cloudtest/constants/constants.js
@@ -1,5 +1,17 @@
 //设置全局变量
 
 export const components_map = {
-  "org.apache.jmeter.testelement.TestPlan": "test-plan",
+  "org.apache.jmeter.control.gui.TestPlanGui_org.apache.jmeter.testelement.TestPlan": "test-plan",
+  "org.apache.jmeter.threads.gui.ThreadGroupGui_org.apache.jmeter.threads.ThreadGroup":"thread-group",
+  "org.apache.jmeter.config.gui.ArgumentsPanel_org.apache.jmeter.config.Arguments":"user-variable",
+  "org.apache.jmeter.testbeans.gui.TestBeanGUI_org.apache.jmeter.config.CSVDataSet":"csvdata-set",
+  "org.apache.jmeter.protocol.http.control.gui.HttpTestSampleGui_org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy":"http-sampler",
+  "org.apache.jmeter.extractor.json.jsonpath.gui.JSONPostProcessorGui_org.apache.jmeter.extractor.json.jsonpath.JSONPostProcessor":"json-extractor",
+  "org.apache.jmeter.visualizers.ViewResultsFullVisualizer_org.apache.jmeter.reporters.ResultCollector":"result-tree",
+  "org.apache.jmeter.visualizers.TableVisualizer_org.apache.jmeter.reporters.ResultCollector":"result-table",
+  "org.apache.jmeter.protocol.http.gui.HeaderPanel_org.apache.jmeter.protocol.http.control.HeaderManager":"http-header",
+  "org.apache.jmeter.assertions.gui.AssertionGui_org.apache.jmeter.assertions.ResponseAssertion":"response-assertion",
+  "org.apache.jmeter.testbeans.gui.TestBeanGUI_org.apache.jmeter.extractor.BeanShellPostProcessor":"beanshell-post",
+  "org.apache.jmeter.testbeans.gui.TestBeanGUI_org.apache.jmeter.modifiers.BeanShellPreProcessor":"beanshell-pre",
+  "org.apache.jmeter.control.gui.ForeachControlPanel_org.apache.jmeter.control.ForeachController":"foreach-controller",
 }
