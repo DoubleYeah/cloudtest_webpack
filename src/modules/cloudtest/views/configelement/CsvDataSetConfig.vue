04c6abb860d3710ab51d5458508a086f55753be9
peoplhappy <39702717+peoplhappy@users.noreply.github.com>
2019-06-12 18:46:37 +0800

    更新

--- src/modules/cloudtest/views/configelement/CsvDataSetConfig.vue
+++ src/modules/cloudtest/views/configelement/CsvDataSetConfig.vue
@@ -14,11 +14,11 @@
         <fieldset>
           <legend>{{input4}}</legend>
           <div class="csvdatasetproperty">
-            <el-input v-model="filename" >
+            <el-input v-model="filename">
               <template slot="prepend">Filename:</template>
               <el-button type="primary" slot="append">Browse...</el-button>
             </el-input>
-            <el-input v-model="fileEncoding" :readonly="editfileencode">
+            <el-input v-model="fileEncodinginput" :readonly="editfileencode">
               <template slot="prepend">File encoding:</template>
               <el-select slot="append" placeholder v-model="fileEncoding">
                 <el-option label="UTF-8" value="UTF-8"></el-option>
@@ -31,12 +31,12 @@
             <el-input v-model="variableNames">
               <template slot="prepend">Variable Names(comma-delimited):</template>
             </el-input>
-            <el-input v-model="input2" placeholder>
+            <el-input v-model="ignoreFirstLineinput" :readonly="editignorefirstline">
               <template slot="prepend">Ignore first line(only used if Variable Names is not Empty):</template>
-              <el-select slot="append" placeholder v-model="input2">
-                <el-option label="True" value="1"></el-option>
-                <el-option label="False" value="2"></el-option>
-                <el-option label="Edit" value="3"></el-option>
+              <el-select slot="append" placeholder v-model="ignoreFirstLine">
+                <el-option label="True" value="true"></el-option>
+                <el-option label="False" value="false"></el-option>
+                <el-option label="Edit" value="Edit"></el-option>
               </el-select>
             </el-input>
             <el-input v-model="delimiter">
@@ -84,109 +84,124 @@
 
 
 <script>
-export default {
-  props: {
-    content: {
-      type: Object,
-      required: true
-    }
-  },
-  name: "CsvDataSetConfig",
-  data() {
-    return {
-      input: "Csv Data Set Config",
-      input2: "",
-      input3: "User Defined Variables",
-      input4: "Configure the Csv Data Source",
-      input5: ",",
-      pagedata: {},
-      elementname: "",
-      comments: "",
-      filename: "",
-      fileEncoding: "",
-      ignoreFirstLine: false,
-      quotedData: false,
-      recycle: true,
-      shareMode: "shareMode.all",
-      stopThread: false,
-      variableNames: "",
-      delimiter: ",",
-      editfileencode:true
-    };
-  },
-  methods: {
-    getContentData() {
-      //var urldata = this.$route.query
-      //this.pagedata = JSON.parse(urldata.content)
-      // props获取来自父控件的数据
-      this.pagedata = this.content.content;
-      this.elementname = this.pagedata["data"]["propMap"]["TestElement.name"][
-        "data"
-      ]["value"];
-      if (this.pagedata["data"]["propMap"]["TestPlan.comments"] == undefined) {
-        this.comments = "";
-      } else {
-        this.comments = this.pagedata["data"]["propMap"]["TestPlan.comments"][
+  export default {
+    props: {
+      content: {
+        type: Object,
+        required: true
+      }
+    },
+    name: "CsvDataSetConfig",
+    data() {
+      return {
+        input: "Csv Data Set Config",
+        input2: "",
+        input3: "User Defined Variables",
+        input4: "Configure the Csv Data Source",
+        input5: ",",
+        pagedata: {},
+        elementname: "",
+        comments: "",
+        filename: "",
+        fileEncoding: "",
+        fileEncodinginput: "",
+        ignoreFirstLine: false,
+        ignoreFirstLineinput: false,
+        quotedData: false,
+        recycle: true,
+        shareMode: "shareMode.all",
+        stopThread: false,
+        variableNames: "",
+        delimiter: ",",
+        editfileencode: true,
+        editignorefirstline: true
+      };
+    },
+    methods: {
+      getContentData() {
+        //var urldata = this.$route.query
+        //this.pagedata = JSON.parse(urldata.content)
+        // props获取来自父控件的数据
+        this.pagedata = this.content.content;
+        this.elementname = this.pagedata["data"]["propMap"]["TestElement.name"][
+          "data"
+        ]["value"];
+        if (this.pagedata["data"]["propMap"]["TestPlan.comments"] == undefined) {
+          this.comments = "";
+        } else {
+          this.comments = this.pagedata["data"]["propMap"]["TestPlan.comments"][
+            "data"
+          ]["value"];
+        }
+        this.delimiter = this.pagedata["data"]["propMap"]["delimiter"]["data"][
+          "value"
+        ];
+        this.fileEncoding = this.pagedata["data"]["propMap"]["fileEncoding"][
+          "data"
+        ]["value"];
+        this.filename = this.pagedata["data"]["propMap"]["filename"]["data"][
+          "value"
+        ];
+        this.ignoreFirstLine = this.pagedata["data"]["propMap"][
+          "ignoreFirstLine"
+        ]["data"]["value"];
+        this.quotedData = this.pagedata["data"]["propMap"]["quotedData"]["data"][
+          "value"
+        ];
+        this.recycle = this.pagedata["data"]["propMap"]["recycle"]["data"][
+          "value"
+        ];
+        this.shareMode = this.pagedata["data"]["propMap"]["shareMode"]["data"][
+          "value"
+        ];
+        this.stopThread = this.pagedata["data"]["propMap"]["stopThread"]["data"][
+          "value"
+        ];
+        this.variableNames = this.pagedata["data"]["propMap"]["variableNames"][
           "data"
         ]["value"];
       }
-      this.delimiter = this.pagedata["data"]["propMap"]["delimiter"]["data"][
-        "value"
-      ];
-      this.fileEncoding = this.pagedata["data"]["propMap"]["fileEncoding"][
-        "data"
-      ]["value"];
-      this.filename = this.pagedata["data"]["propMap"]["filename"]["data"][
-        "value"
-      ];
-      this.ignoreFirstLine = this.pagedata["data"]["propMap"][
-        "ignoreFirstLine"
-      ]["data"]["value"];
-      this.quotedData = this.pagedata["data"]["propMap"]["quotedData"]["data"][
-        "value"
-      ];
-      this.recycle = this.pagedata["data"]["propMap"]["recycle"]["data"][
-        "value"
-      ];
-      this.shareMode = this.pagedata["data"]["propMap"]["shareMode"]["data"][
-        "value"
-      ];
-      this.stopThread = this.pagedata["data"]["propMap"]["stopThread"]["data"][
-        "value"
-      ];
-      this.variableNames = this.pagedata["data"]["propMap"]["variableNames"][
-        "data"
-      ]["value"];
+    },
+    beforeMount() {
+      this.getContentData();
+    },
+    watch: {
+      fileEncoding(val) {
+        if (val == "Edit") {
+          this.editfileencode = false
+          this.fileEncodinginput = ""
+        } else {
+          this.editfileencode = true
+          this.fileEncodinginput = this.fileEncoding
+        }
+      },
+      ignoreFirstLine(val) {
+        if (val == "Edit") {
+          this.editignorefirstline = false
+          this.fileEncodinginput = ""
+        } else {
+          this.editignorefirstline = true
+          this.fileEncodinginput = this.fileEncoding
+        }
+      }
     }
-  },
-  beforeMount() {
-    this.getContentData();
-  },
-  watch:{
-     fileEncoding(val){
-       if(val=="Edit"){
-          this.editfileencode=false
-       }else{
-         this.editfileencode=true
-       }
-     }
-  }
-};
+  };
+
 </script>
 
 <style scoped>
-.el-main {
-  margin-top: 10px;
-}
+  .el-main {
+    margin-top: 10px;
+  }
 
-div .baseproperty {
-}
+  div .baseproperty {}
 
-div .testelementproperty {
-  text-align: center;
-}
+  div .testelementproperty {
+    text-align: center;
+  }
 
-fieldset legend {
-  text-align: left;
-}
+  fieldset legend {
+    text-align: left;
+  }
+
 </style>
